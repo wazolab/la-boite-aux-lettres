0fb8ecf4c644d35cc7af08dece6e7ee3576040ac
Noé Viricel <noe.viricel@wazo-lab.io>
2025-12-01 18:09:58 +0100

    chore: basic @nuxt/content setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  content: {
+    experimental: { nativeSqlite: true }
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxt/content']
 })
