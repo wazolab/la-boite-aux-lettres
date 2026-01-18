fd3b2790f7c0f45c71d98c603865ef9f591bb5f0
Noé Viricel <noe.viricel@wazo-lab.io>
2026-01-18 16:36:17 +0100

    feat: add ci deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  $production: {
+    app: {
+      baseURL: '/la-boite-aux-lettres/'
+    }
+  },
   content: {
     experimental: { nativeSqlite: true }
   },
